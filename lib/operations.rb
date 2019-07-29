13b77e433f9c07d41a7227604c6659543a6c6f84
Darin Webster <darinleenavy@gmail.com>
2019-07-29 19:22:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,11 +8,6 @@ def unsafe?(speed)
   end
 end
 
-
-# def not_safe?(speed)
-#   true ? speed > 60 || speed < 40 : false
-# end
-
 def not_safe?(speed)
-  speed < 60 || speed > 40 ? true : false
+  true ? speed > 60 || speed < 40 : false
 end
